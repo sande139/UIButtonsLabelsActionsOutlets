300f197c7993f4210a196818ade2f1df04f44930
Steven Anderson <sande139@d127.org>
2022-10-17 09:10:10 -0500

    Initial Commit

---
+++ UIButtonsLabelsActionsOutletsChallenge/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  UIButtonsLabelsActionsOutletsChallenge
+//
+//  Created by Robert Griffin III
+//  Copyright (c) 2022 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Start Stretch #2
+    @IBOutlet weak var makemegreen: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Start Stretch #1
+    @IBAction func makemegreen(_ sender: Any) {
+    view.backgroundColor = .green
+    
+    }
+    
+    
+    
+    
+    // MARK: - Start Stretch #3
+    
+    
+
+}
+
